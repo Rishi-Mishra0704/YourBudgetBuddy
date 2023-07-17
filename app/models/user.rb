951dca49b34d462d007c0477428baaabad971e20
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-07-17 13:26:51 +0530

    Add models and migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,22 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  before_create :name_decoration
+  # Associations
+
+  has_many :groups, class_name: 'Group', foreign_key: :user_id, dependent: :destroy
+  has_many :expenses, class_name: 'Expense', foreign_key: :author_id, dependent: :destroy
+
+  # validations
+
+  validates :name, presence: true, length: { maximum: 250, minimum: 2 }
+
+  private
+
+  def name_decoration
+    self.name = name.capitalize if name.blank?
+  end
 end
